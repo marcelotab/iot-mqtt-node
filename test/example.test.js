0fd04656bcbcdb52e25ff33aadd595a8d65bd269
marcelo taborda <mnarcelotaborda72@gmail.com>
2022-08-27 21:54:20 -0300

    starting config jest

---
+++ test/example.test.js
@@ -0,0 +1,14 @@
+// Required app modules.
+// const jest = require("jest");
+
+describe('Testing the operation services', () => {
+
+    describe('Create transaction operation details tests', () => {
+        it('Send to Create a new details FEE, FEE_IVA, PAYER_CFT, PAYER_CFT_IVA With merchant in CABA ', async () => {
+            // Arrange
+
+            // Act
+
+        });
+    });
+});
